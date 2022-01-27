bd18d96441f88a08f31df519b24bacaa3d052700
Arif Faysal <ariffaysal101@gmail.com>
2022-01-27 13:34:19 -0500

    course-class-created

---
+++ src/studentlist/Course.java
@@ -0,0 +1,22 @@
+package studentlist;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Arif Faysal
+ */
+public class Course {
+        private String name;
+        private int id;
+
+    public Course() {
+    }
+    
+    public Course(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+        
+    
+       
+}
