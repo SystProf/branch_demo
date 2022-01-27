f002be0fdfe7bb50157ac64800fe318d09223d5d
megha <megha.patel2@sheridancollege.ca>
2022-01-27 13:12:15 -0500

    megha_parttimestudent branch created parttimestudent.java file added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package studentlist;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class PartTimeStudent extends Student{
+    private int numOfCourses;
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+}
